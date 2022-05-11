aebf92c59c218950ce4e69ea89406289d4ff6af9
mouheb-ria <mouhebabderrahim@gmail.com>
2022-05-11 11:09:43 +0100

    DAHSBOARD

--- resources/views/admin/includes/aside.blade.php
+++ resources/views/admin/includes/aside.blade.php
@@ -1,4 +1,6 @@
+
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
+
     <!-- Brand Logo -->
     <a href="{{ url('/') }}" class="brand-link">
       <img src="{{ asset('assets/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
@@ -13,7 +15,7 @@
           <img src="{{ asset('assets/dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="{{ url('profile') }}" class="d-block">{{ Auth::user()->nom }} {{ Auth::user()->prenom }} </a>
+          <a href="{{ url('profile') }}" style="color: black" class="d-block">{{ Auth::user()->nom }} {{ Auth::user()->prenom }} </a>
         </div>
       </div>
 
