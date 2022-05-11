456e5a42df38d9e2951a3555f01ba2de5c4d871b
mouheb-ria <mouhebabderrahim@gmail.com>
2022-05-11 15:34:14 +0100

    Alert imported

--- app/Http/Controllers/MissionController.php
+++ app/Http/Controllers/MissionController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use Auth;
 use App\Models\Mission;
 use Illuminate\Http\Request;
 use App\Http\Requests\MissionRequest;
-use Auth;
+use RealRashid\SweetAlert\Facades\Alert;
+
 class MissionController extends Controller
 {
 
