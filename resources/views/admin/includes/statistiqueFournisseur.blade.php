f1c350b0131c500676908b2ee372d98fa2c2f534
mouheb-ria <mouhebabderrahim@gmail.com>
2022-05-27 21:07:58 +0100

    KLJQHZD

--- resources/views/admin/includes/statistiqueFournisseur.blade.php
+++ resources/views/admin/includes/statistiqueFournisseur.blade.php
@@ -1,41 +1,7 @@
 
 <div class="row">
-    <div class="col-12 col-sm-6 col-md-3">
-      <a href="{{ url('fournisseur/produits') }}" class="info-box">
-        <span class="info-box-icon bg-info elevation-1">
-        <i class="nav-icon fas fa-book"></i>
-        </span>
-
-        <div class="info-box-content">
-          <span class="info-box-text text-dark">
-              Produits
-          </span>
-          <span class="info-box-number text-dark">
-              {{ App\Models\Produit::count() }}
-          </span>
-        </div>
-        <!-- /.info-box-content -->
-      </a>
-      <!-- /.info-box -->
-    </div>
-    <div class="col-12 col-sm-6 col-md-3">
-      <a href="javascript:void(0)" class="info-box">
-        <span class="info-box-icon bg-info elevation-1">
-        <i class="nav-icon fas fa-book"></i>
-        </span>
-
-        <div class="info-box-content">
-          <span class="info-box-text text-dark">
-              Commandes
-          </span>
-          <span class="info-box-number text-dark">
-              {{ App\Models\Order::count() }}
-          </span>
-        </div>
-        <!-- /.info-box-content -->
-      </a>
-      <!-- /.info-box -->
-    </div>
+   
+    
     <div class="col-12 col-sm-6 col-md-3">
       <a href="javascript:void(0)" class="info-box">
         <span class="info-box-icon bg-info elevation-1">
